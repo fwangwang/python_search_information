71d365754d329ec821b37fc45753834569e586cf
fwang <wm2987642240@gmail.com>
2021-03-07 15:41:33 +0800

    test

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
+class A{
+    int a;
+    int b;
+    long c;
+    char d;
+};
 using namespace std;
 int main()
 {
+    A a;
     cout<<"hello,world!"<<endl;
+    cout<<sizeof(a)<<endl;
     return 0;
 }
